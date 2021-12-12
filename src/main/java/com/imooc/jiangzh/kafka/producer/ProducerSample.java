1ff17052967404a80f5b628a80f0e8301c66fa4f
huohuo <1047984367@qq.com>
2021-12-12 17:59:43 +0800

    初始化学习代码

--- src/main/java/com/imooc/jiangzh/kafka/producer/ProducerSample.java
+++ src/main/java/com/imooc/jiangzh/kafka/producer/ProducerSample.java
@@ -21,20 +21,29 @@ public class ProducerSample {
     }
 
 
-    /*
-        Producer异步发送带回调函数和Partition负载均衡
+    /**
+     *  Producer异步发送带回调函数和Partition负载均衡
      */
     public static void producerSendWithCallbackAndPartition(){
         Properties properties = new Properties();
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"192.168.220.128:9092");
+        //producer 需要 server 接收到数据之后发出的 确认接收的信号，此项配置就是指 procuder 需要多少个这样的确认信号。此配置实际上代表。
+        // acks=0： 设置为 0 表示 producer 不需 要等待任何确认收到的信息
+        // acks=1： 这意味着至少要等待 leader 已经成功将数据写入本地 log，但是并没有等待 所有follower是否成功写入
+        // acks=all： 这意味着 leader 需要等待所 有备份都成功写入日志，这种策略会保证只要有 一个备份存活就不会丢失数据。这是最强的保证。
         properties.put(ProducerConfig.ACKS_CONFIG,"all");
+        //重试次数
         properties.put(ProducerConfig.RETRIES_CONFIG,"0");
+        //批次大小
         properties.put(ProducerConfig.BATCH_SIZE_CONFIG,"16384");
+        //多久发送一个批次到服务端
         properties.put(ProducerConfig.LINGER_MS_CONFIG,"1");
+        //缓存最大
         properties.put(ProducerConfig.BUFFER_MEMORY_CONFIG,"33554432");
-
+        //指定序列化类
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
+        //指定分区负载类
         properties.put(ProducerConfig.PARTITIONER_CLASS_CONFIG,"com.imooc.jiangzh.kafka.producer.SamplePartition");
 
         // Producer的主对象
@@ -48,8 +57,7 @@ public class ProducerSample {
             producer.send(record, new Callback() {
                 @Override
                 public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-                    System.out.println(
-                            "partition : "+recordMetadata.partition()+" , offset : "+recordMetadata.offset());
+                    System.out.println("partition : "+recordMetadata.partition()+" , offset : "+recordMetadata.offset());
                 }
             });
         }
@@ -78,14 +86,13 @@ public class ProducerSample {
 
         // 消息对象 - ProducerRecoder
         for(int i=0;i<10;i++){
-            ProducerRecord<String,String> record =
-                    new ProducerRecord<>(TOPIC_NAME,"key-"+i,"value-"+i);
+            ProducerRecord<String,String> record = new ProducerRecord<>(TOPIC_NAME,"key-"+i,"value-"+i);
 
+            //发送完成回调
             producer.send(record, new Callback() {
                 @Override
                 public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-                    System.out.println(
-                            "partition : "+recordMetadata.partition()+" , offset : "+recordMetadata.offset());
+                    System.out.println("partition : "+recordMetadata.partition()+" , offset : "+recordMetadata.offset());
                 }
             });
         }
@@ -99,14 +106,21 @@ public class ProducerSample {
      */
     public static void producerSend(){
         Properties properties = new Properties();
+        //服务地址
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"192.168.220.128:9092");
+        //
         properties.put(ProducerConfig.ACKS_CONFIG,"all");
+        //重试
         properties.put(ProducerConfig.RETRIES_CONFIG,"0");
+        //批量大小
         properties.put(ProducerConfig.BATCH_SIZE_CONFIG,"16384");
+        //
         properties.put(ProducerConfig.LINGER_MS_CONFIG,"1");
+        //
         properties.put(ProducerConfig.BUFFER_MEMORY_CONFIG,"33554432");
-
+        //key 序列化方式
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
+        //value 序列化方式
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
 
         // Producer的主对象
@@ -114,9 +128,8 @@ public class ProducerSample {
 
         // 消息对象 - ProducerRecoder
         for(int i=0;i<10;i++){
-            ProducerRecord<String,String> record =
-                    new ProducerRecord<>(TOPIC_NAME,"key-"+i,"value-"+i);
-
+            //创建需要发送的消息记录
+            ProducerRecord<String,String> record = new ProducerRecord<>(TOPIC_NAME,"key-"+i,"value-"+i);
             producer.send(record);
         }
 
@@ -124,8 +137,8 @@ public class ProducerSample {
         producer.close();
     }
 
-    /*
-        Producer异步阻塞发送
+    /**
+     *  Producer异步阻塞发送/同步发送
      */
     public static void producerSyncSend() throws ExecutionException, InterruptedException {
         Properties properties = new Properties();
@@ -145,11 +158,11 @@ public class ProducerSample {
         // 消息对象 - ProducerRecoder
         for(int i=0;i<10;i++){
             String key = "key-"+i;
-            ProducerRecord<String,String> record =
-                    new ProducerRecord<>(TOPIC_NAME,key,"value-"+i);
-
+            ProducerRecord<String,String> record = new ProducerRecord<>(TOPIC_NAME,key,"value-"+i);
             Future<RecordMetadata> send = producer.send(record);
+
             RecordMetadata recordMetadata = send.get();
+            //1 接受目标分片 2 位置
             System.out.println(key + "partition : "+recordMetadata.partition()+" , offset : "+recordMetadata.offset());
         }
 
