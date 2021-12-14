4905d4c910303903a9fadcd72a88f8c1e8fdf0d3
huohuo <1047984367@qq.com>
2021-12-15 01:26:25 +0800

    初始化学习代码

--- src/main/java/com/imooc/jiangzh/kafka/KafkaStudyApplication.java
+++ src/main/java/com/imooc/jiangzh/kafka/KafkaStudyApplication.java
@@ -16,6 +16,7 @@ public class KafkaStudyApplication {
      * 3 生产者随便访问哪个节点都可发送数据（有同步机制）
      *
      * ISR节点列表 acks=1 时 同步等待响应同步完成的节点列表 当leader 挂了会在ISR中选择最快的节点
+     * ISR如果全部宕机就会出现脏选举
      * controller 节点中注册zookeeper最快的节点 作用 管理broker节点，选举替换leader
      * @param args
      */
